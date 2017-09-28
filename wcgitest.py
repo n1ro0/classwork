09a4dc76812a8bc6f164cdc48d81d60a5903ddef
n1ro0 <n1ro0ilya@gmail.com>
2017-09-28 17:02:20 +0300

    wsgi

--- wcgitest.py
+++ wcgitest.py
@@ -1,10 +1,26 @@
 from wsgiref.simple_server import make_server
+from urllib.parse import parse_qs
+from html import escape
 
 
 # Web Server Gateway Interface
+#p = path_info.split('/')
+#if p[0] = 'path':
+#id = p[1]
+#/......    /?hash=....
 def app(environ, start_response):
     start_response("200 OK", [("Content-Type", "text/plain")])
-    return [('%s=%s\n'% (k, v)).encode("UTF-8") for k, v in environ.items()]
-
+    #start_response("301 ...", [("Location", "text/plain")])
+    #return []
+    qs = parse_qs(environ['QUERY_STRING'])
+    #return [('%s=%s\n'% (k, v)).encode("UTF-8") for k, v in environ.items()]
+    try:
+        size = int(environ.get('CONTENT_LENGTH', 0))
+    except ValueError:
+        size = 0
+    data = environ['wsgi.input'].read(size)
+    data = parse_qs(data)
+    a = escape(qs.get('a', [""])[0])
+    return [('%s = %s\n' % (k, v)).encode("UTF-8") for k, v in data.items()]
 httpserv = make_server("localhost", 8080, app)
 httpserv.serve_forever()
